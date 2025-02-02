7481d642b4f93842efe8e29849d50fcf206eef70
lemon <l3mon@lemons-MacBook-Air.local>
2025-02-02 13:16:39 +0530

    fixing reset password feature

--- backendv1/src/controllers/authControllers.js
+++ backendv1/src/controllers/authControllers.js
@@ -1,30 +1,28 @@
 const User = require("../models/User");
 const { registerUserHelper, loginUserHelper } = require("../utils/authHelpers");
-const { z } = require('zod');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const emailjs = require('emailjs');
-const axios = require('axios')
+const { z } = require("zod");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const emailjs = require("emailjs");
+const axios = require("axios");
 require("dotenv").config();
-const {Resent, Resend} = require('resend')
-
+const { Resent, Resend } = require("resend");
 
 const registerUser = async (req, res) => {
-
   const registerBody = z.object({
     email: z.string().email().min(6).max(100),
     name: z.string().min(3).max(100),
-    password: z.string().min(6).max(20)
-  })
+    password: z.string().min(6).max(20),
+  });
 
   try {
-    const parseData = registerBody.safeParse(req.body)
-    if(!parseData.success){
+    const parseData = registerBody.safeParse(req.body);
+    if (!parseData.success) {
       res.status(404).json({
-        message:"Somethings wrong please try again",
-        error: parseData.error.issues.map((issue) => issue.message )
-      })
-      return
+        message: "Somethings wrong please try again",
+        error: parseData.error.issues.map((issue) => issue.message),
+      });
+      return;
     }
     const { name, password, email } = parseData.data;
 
@@ -44,22 +42,21 @@ const registerUser = async (req, res) => {
 };
 
 const loginUser = async (req, res) => {
-  
   try {
     const loginBody = z.object({
-      email:z.string().email().min(6).max(30),
-      password: z.string().min(6).max(30)
-    })
+      email: z.string().email().min(6).max(30),
+      password: z.string().min(6).max(30),
+    });
 
-    const parseData = loginBody.safeParse(req.body)
-    if(!parseData.success){
+    const parseData = loginBody.safeParse(req.body);
+    if (!parseData.success) {
       res.status(404).json({
-        message:"Somethings wrong please try again",
-        error: parseData.error.issues.map((issue) => issue.message )
-      })
-      return
+        message: "Somethings wrong please try again",
+        error: parseData.error.issues.map((issue) => issue.message),
+      });
+      return;
     }
-    const {  email, password } = parseData.data;
+    const { email, password } = parseData.data;
     const token = await loginUserHelper(email, password);
     res.status(200).json({ token: token });
   } catch (err) {
@@ -67,86 +64,86 @@ const loginUser = async (req, res) => {
   }
 };
 
-const requestPasswordReset = async(req, res)=>{
-  try{
-
+const requestPasswordReset = async (req, res) => {
+  try {
     const { email } = req.body;
     //finding user form db
-    const user = await User.findOne({email})
-    console.log(user)
+    const user = await User.findOne({ email });
+    console.log(user);
 
-    if(!user){
-      return res.status(404).json({message: "Unexpected error occured"})
+    if (!user) {
+      return res.status(404).json({ message: "Unexpected error occured" });
     }
 
-    console.log("before signing tokens")
+    console.log("before signing tokens");
     const secret = process.env.SECRET + user.password;
-    const token = jwt.sign({ id: user._id, email: user.email }, secret, { expiresIn: '1h' });
+    const token = jwt.sign({ id: user._id, email: user.email }, secret, {
+      expiresIn: "1h",
+    });
 
-    console.log("after signing tokens")
+    console.log("after signing tokens");
     //crafting reset link to send to user via mail
-    const resetURL = `http://localhost:3000/auth/resetpassword?id=${user._id}&token=${token}`
+    const resetURL = `https://todo-fullstack-sape.onrender.com/auth/resetpassword?id=${user._id}&token=${token}`;
 
-    console.log("creafted the reset url")
-    
-    const resend = new Resend(process.env.RESENT_API)
+    console.log("creafted the reset url");
+
+    const resend = new Resend(process.env.RESENT_API);
 
     const mail = await resend.emails.send({
-        from: 'tofusupoort@resend.dev',
-        to: 'alvinliju44@gmail.com',
-        subject: 'Hello World',
-        html: `<p>Your reset link is:${resetURL} </strong>!</p>`
-    })
+      from: "tofusupoort@resend.dev",
+      to: "alvinliju44@gmail.com",
+      subject: "Hello World",
+      html: `<p>Your reset link is:${resetURL} </strong>!</p>`,
+    });
 
-    if(!mail) res.status(500).json({message:"somethfdk fuck off"})
+    if (!mail) res.status(500).json({ message: "somethfdk fuck off" });
 
-      res.status(200).json({message:"Email send"})
-    
-   
-  }catch(err){
-    res.status(500).json({message: "something went wrong"})
+    res.status(200).json({ message: "Email send" });
+  } catch (err) {
+    res.status(500).json({ message: "something went wrong" });
   }
-}
+};
 
 //reset password
-const resetPassword = async (req,res) => {
-  try{
-    console.log("here")
-    const {id, token} = req.query
-    const {password} = req.body
+const resetPassword = async (req, res) => {
+  try {
+    console.log("here");
+    const { id, token } = req.query;
+    const { password } = req.body;
 
-    console.log("form reset pass")
-    console.log(id, token, password)
-    const user = await User.findById({_id:id})
-    console.log(user)
+    console.log("form reset pass");
+    console.log(id, token, password);
+    const user = await User.findById({ _id: id });
+    console.log(user);
 
-    if(!user){
+    if (!user) {
       return res.status(400).json({ message: "User not exists!" });
     }
 
     const secret = process.env.SECRET + user.password;
     const verify = jwt.verify(token, secret);
-    if(!verify) res.status(404).json({message:"unauthorized access"})
+    if (!verify) res.status(404).json({ message: "unauthorized access" });
     const hashedPassword = await bcrypt.hash(password, 10);
     await User.updateOne(
       {
-      _id: id
-    },
-    {
-      $set: {
-        password: hashedPassword
-      }
-    }
-  )
+        _id: id,
+      },
+      {
+        $set: {
+          password: hashedPassword,
+        },
+      },
+    );
 
-  res.status(200).json({message:"Password has been reset"})
-    
-  }catch(err){
-    res.status(500).json({message: "something went wrong"})
+    res.status(200).json({ message: "Password has been reset" });
+  } catch (err) {
+    res.status(500).json({ message: "something went wrong" });
   }
+};
 
-}
-
-
-
-module.exports = { registerUser, loginUser, resetPassword, requestPasswordReset };
+module.exports = {
+  registerUser,
+  loginUser,
+  resetPassword,
+  requestPasswordReset,
+};
